svn2github/dart-lang

commit 270956264a6eb9bc1bb188873ecdc88df06fa0a1

Add support for https scheme in dart:io HttpClient.
BUG=

Review URL: https://codereview.chromium.org//11299210

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@15450 260f80e4-7a28-3924-810f-c04153c831b5

dart/sdk/lib/io/http_impl.dart
@@ -1704,7 +1704,7 @@ class _HttpClient implements HttpClient {
   HttpClientConnection _openUrl(String method,
                                 Uri url,
                                 [_HttpClientConnection connection]) {
-    if (url.scheme != "http") {
+    if (url.scheme != "http" && url.scheme != "https") {
       throw new HttpException("Unsupported URL scheme ${url.scheme}");
     }
     return _open(method, url, connection);
@@ -1769,7 +1769,8 @@ class _HttpClient implements HttpClient {
                               int port,
                               _ProxyConfiguration proxyConfiguration,
                               int proxyIndex,
-                              bool reusedConnection) {
+                              bool reusedConnection,
+                              bool secure) {
 
       void _connectionOpened(_SocketConnection socketConn,
                              _HttpClientConnection connection,
@@ -1821,8 +1822,15 @@ class _HttpClient implements HttpClient {
       // otherwise create a new one.
       String key = _connectionKey(connectHost, connectPort);
       Queue socketConnections = _openSockets[key];
+      // Remove active connections that are of the wrong type (HTTP or HTTPS).
+      while (socketConnections != null &&
+             !socketConnections.isEmpty &&
+             secure != (socketConnections.first._socket is SecureSocket)) {
+        socketConnection.removeFirst()._close();
+      }
       if (socketConnections == null || socketConnections.isEmpty) {
-        Socket socket = new Socket(connectHost, connectPort);
+        Socket socket = secure ? new SecureSocket(connectHost, connectPort) :
+                                 new Socket(connectHost, connectPort);
         // Until the connection is established handle connection errors
         // here as the HttpClientConnection object is not yet associated
         // with the socket.
@@ -1845,7 +1853,7 @@ class _HttpClient implements HttpClient {
           // the connected socket.
           socket.onError = null;
           _SocketConnection socketConn =
-          new _SocketConnection(connectHost, connectPort, socket);
+              new _SocketConnection(connectHost, connectPort, socket);
           _activeSockets.add(socketConn);
           _connectionOpened(socketConn, connection, !proxy.isDirect);
         };
@@ -1861,10 +1869,17 @@ class _HttpClient implements HttpClient {
       }
     }
 
+    // Find out if we want a secure socket.
+    bool secure = (url.scheme == "https");
+
     // Find the TCP host and port.
     String host = url.domain;
-    int port = url.port == 0 ? HttpClient.DEFAULT_HTTP_PORT : url.port;
-
+    int port = url.port;
+    if (port == 0) {
+      port = secure ?
+          HttpClient.DEFAULT_HTTPS_PORT :
+          HttpClient.DEFAULT_HTTP_PORT;
+    }
     // Create a new connection object if we are not re-using an existing one.
     var reusedConnection = false;
     if (connection == null) {
@@ -1883,7 +1898,12 @@ class _HttpClient implements HttpClient {
     }
 
     // Establish the connection starting with the first proxy configured.
-    _establishConnection(host, port, proxyConfiguration, 0, reusedConnection);
+    _establishConnection(host,
+                         port,
+                         proxyConfiguration,
+                         0,
+                         reusedConnection,
+                         secure);
 
     return connection;
   }

dart/sdk/lib/io/http.dart
@@ -734,6 +734,7 @@ abstract class HttpResponse {
  */
 abstract class HttpClient {
   static const int DEFAULT_HTTP_PORT = 80;
+  static const int DEFAULT_HTTPS_PORT = 443;
 
   factory HttpClient() => new _HttpClient();
 

dart/sdk/lib/io/secure_socket.dart
@@ -169,7 +169,7 @@ class _SecureSocket implements SecureSocket {
     throw new UnimplementedError("SecureSocket.available not implemented yet");
   }
 
-  void close([bool halfClose]) {
+  void close([bool halfClose = false]) {
     if (halfClose) {
       _closedWrite = true;
       _writeEncryptedData();

dart/tests/standalone/io/https_client_test.dart
@@ -1,0 +1,66 @@
+// Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import "dart:io";
+import "dart:uri";
+import "dart:isolate";
+
+
+int testGoogleUrlCount = 0;
+void testGoogleUrl() {
+  HttpClient client = new HttpClient();
+
+  void testUrl(String url) {
+    var requestUri = new Uri.fromString(url);
+    var conn = client.getUrl(requestUri);
+
+    conn.onRequest = (HttpClientRequest request) {
+      request.outputStream.close();
+    };
+    conn.onResponse = (HttpClientResponse response) {
+      testGoogleUrlCount++;
+      Expect.isTrue(response.statusCode < 500);
+      if (requestUri.path.length == 0) {
+        Expect.isTrue(response.statusCode != 404);
+      }
+      response.inputStream.onData = () {
+        response.inputStream.read();
+      };
+      response.inputStream.onClosed = () {
+        if (testGoogleUrlCount == 4) client.shutdown();
+      };
+    };
+    conn.onError = (error) => Expect.fail("Unexpected IO error $error");
+  }
+
+  testUrl('https://www.google.dk');
+  testUrl('https://www.google.dk');
+  testUrl('https://www.google.dk/#q=foo');
+  testUrl('https://www.google.dk/#hl=da&q=foo');
+}
+
+void testBadHostName() {
+  HttpClient client = new HttpClient();
+  HttpClientConnection connection = client.getUrl(
+      new Uri.fromString("https://some.bad.host.name.7654321/"));
+  connection.onRequest = (HttpClientRequest request) {
+    Expect.fail("Should not open a request on bad hostname");
+  };
+  ReceivePort port = new ReceivePort();
+  connection.onError = (Exception error) {
+    port.close();  // We expect onError to be called, due to bad host name.
+  };
+}
+
+void InitializeSSL() {
+  var testPkcertDatabase =
+      new Path.fromNative(new Options().script).directoryPath.append('pkcert/');
+  SecureSocket.setCertificateDatabase(testPkcertDatabase.toNativePath());
+}
+
+void main() {
+  InitializeSSL();
+  testGoogleUrl();
+  testBadHostName();
+}

commit 33a098592a3cd62d930d9d88aff668a3b3e7ec89

Fix bug in HttpClient, introduced in r15450.
BUG=

Review URL: https://codereview.chromium.org//11415176

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@15451 260f80e4-7a28-3924-810f-c04153c831b5

dart/sdk/lib/io/http_impl.dart
@@ -1839,7 +1839,7 @@ class _HttpClient implements HttpClient {
           if (proxyIndex < proxyConfiguration.proxies.length) {
             // Try the next proxy in the list.
             _establishConnection(
-                host, port, proxyConfiguration, proxyIndex, false);
+                host, port, proxyConfiguration, proxyIndex, false, secure);
           } else {
             // Report the error through the HttpClientConnection object to
             // the client.
@@ -1870,13 +1870,13 @@ class _HttpClient implements HttpClient {
     }
 
     // Find out if we want a secure socket.
-    bool secure = (url.scheme == "https");
+    bool is_secure = (url.scheme == "https");
 
     // Find the TCP host and port.
     String host = url.domain;
     int port = url.port;
     if (port == 0) {
-      port = secure ?
+      port = is_secure ?
           HttpClient.DEFAULT_HTTPS_PORT :
           HttpClient.DEFAULT_HTTP_PORT;
     }
@@ -1903,7 +1903,7 @@ class _HttpClient implements HttpClient {
                          proxyConfiguration,
                          0,
                          reusedConnection,
-                         secure);
+                         is_secure);
 
     return connection;
   }