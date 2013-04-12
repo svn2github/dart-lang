svn2github/dart-lang

commit 7aac2e75d4939ab2f85b6d1620e286750f1bd5e8

Fix bug in MegamorphicCacheMissHandler: put kNullCid into the cache when receiver is null.
We were putting kObjectCid before causing inline lookup code to always miss when receiver is null.

BUG=dart:9815

Review URL: https://codereview.chromium.org//13951007

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@21366 260f80e4-7a28-3924-810f-c04153c831b5

dart/runtime/vm/code_generator.cc
@@ -1014,8 +1014,9 @@ DEFINE_RUNTIME_ENTRY(MegamorphicCacheMissHandler, 3) {
   const MegamorphicCache& cache = MegamorphicCache::Handle(
       isolate->megamorphic_cache_table()->Lookup(name, descriptor));
   Class& cls = Class::Handle(receiver.clazz());
+  const bool is_null = cls.IsNullClass();
   // For lookups treat null as an instance of class Object.
-  if (cls.IsNullClass()) {
+  if (is_null) {
     cls = isolate->object_store()->object_class();
   }
   ASSERT(!cls.IsNull());
@@ -1049,7 +1050,7 @@ DEFINE_RUNTIME_ENTRY(MegamorphicCacheMissHandler, 3) {
   if (instructions.IsNull()) return;
 
   cache.EnsureCapacity();
-  const Smi& class_id = Smi::Handle(Smi::New(cls.id()));
+  const Smi& class_id = Smi::Handle(Smi::New(is_null ? kNullCid : cls.id()));
   cache.Insert(class_id, target);
   return;
 }

commit 2df7734ba79b88c5f81840921669a3bfa7e66cd2

Fix x64 build failing after r21366.
TBR=iposva@google.com

Review URL: https://codereview.chromium.org//13975010

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@21367 260f80e4-7a28-3924-810f-c04153c831b5

dart/runtime/vm/code_generator.cc
@@ -1050,7 +1050,8 @@ DEFINE_RUNTIME_ENTRY(MegamorphicCacheMissHandler, 3) {
   if (instructions.IsNull()) return;
 
   cache.EnsureCapacity();
-  const Smi& class_id = Smi::Handle(Smi::New(is_null ? kNullCid : cls.id()));
+  const Smi& class_id = Smi::Handle(Smi::New(
+      is_null ? static_cast<intptr_t>(kNullCid) : cls.id()));
   cache.Insert(class_id, target);
   return;
 }