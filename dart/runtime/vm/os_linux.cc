svn2github/dart-lang

commit 7bdabba9117ea865fce6d576c4d78d912653568a

- Write the coverage data to a file instead of printing to stdout. - Explicitly include lines with zero hits.
R=asiva@google.com, mlippautz@google.com

Review URL: https://codereview.chromium.org//24255013

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@27726 260f80e4-7a28-3924-810f-c04153c831b5

dart/runtime/vm/os_linux.cc
@@ -129,10 +129,10 @@ class PerfCodeObserver : public CodeObserver {
     if (file_open == NULL) {
       return;
     }
-    const char* format = "/tmp/perf-%ld.map";
+    const char* format = "/tmp/perf-%"Pd".map";
     intptr_t pid = getpid();
     intptr_t len = OS::SNPrint(NULL, 0, format, pid);
-    char* filename = new char[len + 1];
+    char* filename = Isolate::Current()->current_zone()->Alloc<char>(len + 1);
     OS::SNPrint(filename, len + 1, format, pid);
     out_file_ = (*file_open)(filename, true);
   }
@@ -270,6 +270,11 @@ class GdbCodeObserver : public CodeObserver {
 
 const char* OS::Name() {
   return "linux";
+}
+
+
+intptr_t OS::ProcessId() {
+  return static_cast<intptr_t>(getpid());
 }
 
 

dart/runtime/vm/class_finalizer.cc
@@ -1735,7 +1735,7 @@ void ClassFinalizer::FinalizeClass(const Class& cls) {
   cls.Finalize();
   // Mixin typedef classes may still lack their implicit constructor.
   // TODO(regis): Implement mixin typedefs with an alias class.
-  if (cls.is_synthesized_class() &&
+  if (cls.is_mixin_typedef() &&
       (cls.functions() == Object::empty_array().raw())) {
     Parser::AddImplicitConstructor(cls);
   }

dart/runtime/vm/coverage.cc
@@ -4,6 +4,8 @@
 
 #include "vm/coverage.h"
 
+#include "include/dart_api.h"
+
 #include "vm/isolate.h"
 #include "vm/json_stream.h"
 #include "vm/object.h"
@@ -11,7 +13,8 @@
 
 namespace dart {
 
-DEFINE_FLAG(bool, print_coverage, false, "Print code coverage.");
+DEFINE_FLAG(charp, coverage_dir, NULL,
+            "Enable writing coverage data into specified directory.");
 
 void CodeCoverage::PrintClass(const Class& cls, const JSONArray& jsarr) {
   const Array& functions = Array::Handle(cls.functions());
@@ -26,20 +29,22 @@ void CodeCoverage::PrintClass(const Class& cls, const JSONArray& jsarr) {
   ICData& ic_data = ICData::Handle();
   for (int i = 0; i < functions.Length(); i++) {
     function ^= functions.At(i);
+
+    JSONObject jsobj(&jsarr);
+    script = function.script();
+    url = script.url();
+    name = function.QualifiedUserVisibleName();
+    jsobj.AddProperty("source", url.ToCString());
+    jsobj.AddProperty("function", name.ToCString());
+
+    JSONArray jsarr(&jsobj, "hits");
+
     if (function.HasCode()) {
-      JSONObject jsobj(&jsarr);
-
-      script = function.script();
-      url = script.url();
-      name = function.QualifiedUserVisibleName();
-      jsobj.AddProperty("source", url.ToCString());
-      jsobj.AddProperty("function", name.ToCString());
-
+      // Print the hit counts for all IC datas.
       code = function.unoptimized_code();
       ic_array = code.ExtractTypeFeedbackArray();
       descriptors = code.pc_descriptors();
 
-      JSONArray jsarr(&jsobj, "hits");
       for (int j = 0; j < descriptors.Length(); j++) {
         PcDescriptors::Kind kind = descriptors.DescriptorKind(j);
         // Only IC based calls have counting.
@@ -47,7 +52,7 @@ void CodeCoverage::PrintClass(const Class& cls, const JSONArray& jsarr) {
             (kind == PcDescriptors::kUnoptStaticCall)) {
           intptr_t deopt_id = descriptors.DeoptId(j);
           ic_data ^= ic_array.At(deopt_id);
-          if (!ic_data.IsNull() && (ic_data.AggregateCount() > 0)) {
+          if (!ic_data.IsNull()) {
             intptr_t token_pos = descriptors.TokenPos(j);
             intptr_t line = -1;
             intptr_t col = -1;
@@ -59,14 +64,34 @@ void CodeCoverage::PrintClass(const Class& cls, const JSONArray& jsarr) {
           }
         }
       }
+    } else {
+      // The function has no code so it was never executed and thus we add one
+      // zero count hit at the first token index.
+      intptr_t line = -1;
+      intptr_t col = -1;
+      script.GetTokenLocation(function.token_pos(), &line, &col);
+      JSONObject func_info(&jsarr);
+      func_info.AddProperty("line", line);
+      func_info.AddProperty("col", col);
+      func_info.AddProperty("count", static_cast<intptr_t>(0));
     }
   }
 }
 
 
-void CodeCoverage::Print(Isolate* isolate) {
-  JSONStream stream;
+void CodeCoverage::Write(Isolate* isolate) {
+  if (FLAG_coverage_dir == NULL) {
+    return;
+  }
 
+  Dart_FileOpenCallback file_open = Isolate::file_open_callback();
+  Dart_FileWriteCallback file_write = Isolate::file_write_callback();
+  Dart_FileCloseCallback file_close = Isolate::file_close_callback();
+  if ((file_open == NULL) || (file_write == NULL) || (file_close == NULL)) {
+    return;
+  }
+
+  JSONStream stream;
   {
     const GrowableObjectArray& libs = GrowableObjectArray::Handle(
         isolate, isolate->object_store()->libraries());
@@ -87,9 +112,20 @@ void CodeCoverage::Print(Isolate* isolate) {
     }
   }
 
-  OS::Print("### COVERAGE DATA ###\n"
-            "%s\n"
-            "### END ###\n", stream.ToCString());
+  const char* format = "%s/dart-cov-%"Pd"-%"Pd".json";
+  intptr_t pid = OS::ProcessId();
+  intptr_t len = OS::SNPrint(NULL, 0, format,
+                             FLAG_coverage_dir, pid, isolate->main_port());
+  char* filename = Isolate::Current()->current_zone()->Alloc<char>(len + 1);
+  OS::SNPrint(filename, len + 1, format,
+              FLAG_coverage_dir, pid, isolate->main_port());
+  void* file = (*file_open)(filename, true);
+  if (file == NULL) {
+    OS::Print("Failed to write coverage file: %s\n", filename);
+    return;
+  }
+  (*file_write)(stream.buffer()->buf(), stream.buffer()->length(), file);
+  (*file_close)(file);
 }
 
 }  // namespace dart

dart/runtime/vm/coverage.h
@@ -10,7 +10,7 @@
 
 namespace dart {
 
-DECLARE_FLAG(bool, print_coverage);
+DECLARE_FLAG(charp, coverage_dir);
 
 // Forward declarations.
 class Class;
@@ -19,7 +19,7 @@ class JSONArray;
 
 class CodeCoverage : public AllStatic {
  public:
-  static void Print(Isolate* isolate);
+  static void Write(Isolate* isolate);
 
  private:
   static void PrintClass(const Class& cls, const JSONArray& arr);

dart/runtime/vm/isolate.cc
@@ -785,9 +785,8 @@ void Isolate::Shutdown() {
       PrintInvokedFunctions();
     }
 
-    if (FLAG_print_coverage) {
-      CodeCoverage::Print(this);
-    }
+    // Write out the coverage data if collection has been enabled.
+    CodeCoverage::Write(this);
 
     // Finalize any weak persistent handles with a non-null referent.
     FinalizeWeakPersistentHandlesVisitor visitor;

dart/runtime/vm/json_stream.h
@@ -19,6 +19,7 @@ class JSONStream : ValueObject {
   explicit JSONStream(intptr_t buf_size = 256);
   ~JSONStream();
 
+  TextBuffer* buffer() { return &buffer_; }
   const char* ToCString() { return buffer_.buf(); }
 
   void SetArguments(const char** arguments, intptr_t num_arguments);

dart/runtime/vm/object.cc
@@ -64,7 +64,7 @@ DECLARE_FLAG(bool, trace_compiler);
 DECLARE_FLAG(bool, trace_deoptimization);
 DECLARE_FLAG(bool, trace_deoptimization_verbose);
 DECLARE_FLAG(bool, verbose_stacktrace);
-DECLARE_FLAG(bool, print_coverage);
+DECLARE_FLAG(charp, coverage_dir);
 
 static const char* kGetterPrefix = "get:";
 static const intptr_t kGetterPrefixLength = strlen(kGetterPrefix);
@@ -4166,7 +4166,8 @@ void Function::SetNumOptionalParameters(intptr_t num_optional_parameters,
 
 
 bool Function::is_optimizable() const {
-  if (FLAG_print_coverage) {
+  if (FLAG_coverage_dir != NULL) {
+    // Do not optimize if collecting coverage data.
     return false;
   }
   if (OptimizableBit::decode(raw_ptr()->kind_tag_) &&

dart/runtime/vm/os.h
@@ -21,6 +21,9 @@ class OS {
   // Returns the name of the given OS. For example "linux".
   static const char* Name();
 
+  // Returns the current process id.
+  static intptr_t ProcessId();
+
   // Returns the abbreviated time-zone name for the given instant.
   // For example "CET" or "CEST".
   static const char* GetTimeZoneName(int64_t seconds_since_epoch);

commit 16d2378ef55df8bec7687f3af0a963352a23faec

Fix Linux perf profiling support.
This was broken after r27726 because Isolate::Current() is
NULL when the PerfCodeObserver is initialized.

R=kmillikin@google.com

Review URL: https://codereview.chromium.org//24364002

git-svn-id: https://dart.googlecode.com/svn/branches/bleeding_edge@27760 260f80e4-7a28-3924-810f-c04153c831b5

dart/runtime/vm/os_linux.cc
@@ -132,9 +132,10 @@ class PerfCodeObserver : public CodeObserver {
     const char* format = "/tmp/perf-%"Pd".map";
     intptr_t pid = getpid();
     intptr_t len = OS::SNPrint(NULL, 0, format, pid);
-    char* filename = Isolate::Current()->current_zone()->Alloc<char>(len + 1);
+    char* filename = new char[len + 1];
     OS::SNPrint(filename, len + 1, format, pid);
     out_file_ = (*file_open)(filename, true);
+    delete[] filename;
   }
 
   ~PerfCodeObserver() {